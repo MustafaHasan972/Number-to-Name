c2acbe4b553210478f92d5f9e9684cd4a022e36d
Mustafa Hasan <mustafa2k8972@gmail.com>
2023-09-21 22:59:59 +0530

    Created a function to spell out a group of three digits

--- app.js
+++ app.js
@@ -7,4 +7,61 @@ const sayNumberInEnglish = (n) => {
     groups.unshift(group); // This will add the group to the beginning of array groups
     remainingDigits = Math.floor(remainingNumbers / 1000); // This will remove the last three digits from remainingNumbers
   }
+  const spelledGroup = (group) => {
+    const ones = [
+      "",
+      "one",
+      "two",
+      "three",
+      "four",
+      "five",
+      "six",
+      "seven",
+      "eight",
+      "nine",
+    ];
+    const teens = [
+      "ten",
+      "eleven",
+      "twelve",
+      "thirteen",
+      "fourteen",
+      "fifteen",
+      "sixteen",
+      "seventeen",
+      "eighteen",
+      "nineteen",
+    ];
+    const tens = [
+      "",
+      "",
+      "twenty",
+      "thirty",
+      "forty",
+      "fifty",
+      "sixty",
+      "seventy",
+      "eighty",
+      "ninety",
+    ];
+
+    let spellGroup = ""; // This will store the spelled-out representation of the group
+
+    const hundredsDigit = Math.floor(group / 100);
+    const tensDigit = Math.floor((group % 100) / 10);
+    const onesDigit = group % 10;
+
+    if (hundredsDigit > 0) {
+      spelledGroup += ones[hundredsDigit] + " hundred ";
+    }
+
+    if (tensDigit === 1) {
+      spelledGroup += tens[tensDigit] + " ";
+    } else {
+      spellGroup += tens[tensDigit] + " ";
+      spellGroup += ones[onesDigit] + " ";
+    }
+
+    return spelledGroup.trim();
+  };
 };
