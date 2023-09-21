20bf2ccd021c27c496d0fe9ae35ffc8037365c82
Mustafa Hasan <mustafa2k8972@gmail.com>
2023-09-21 22:40:48 +0530

    Updated readme file

---
+++ README.md
@@ -0,0 +1,3 @@
+#Number to Name
+
+This program will spell the numbers ranging from 0 to 999,999,999,999 in English
--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const sayNumberInEnglish = (n) => {
   let groups = [];
   let remainingNumbers = n;
   while (remainingNumbers > 0) {
-    let group = remainingNumbers % 1000; //This will get us the last three digits of remainingNumbers
-    groups.unshift(group);
+    let group = remainingNumbers % 1000; // This will get us the last three digits of remainingNumbers
+    groups.unshift(group); // This will add the group to the beginning of array groups
+    remainingDigits = Math.floor(remainingNumbers / 1000); // This will remove the last three digits from remainingNumbers
   }
 };
